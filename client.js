dbfbf3c2073e8e6b730af345832fd58c8118d12c
anoff <offenhaeuser@gmail.com>
2017-08-27 22:34:16 +0200

    :star2: feat: make client autoretry

--- client.js
+++ client.js
@@ -3,15 +3,35 @@
  * Local dummy client for debug driven development
  */
 const WebSocket = require('ws')
+const log = require('./lib/logger').log
+const RETRY_INTERVAL = 1 // [s]
 
-const ws = new WebSocket('ws://localhost:8080')
+let ws
+function connect () {
+  const ws = new WebSocket('ws://localhost:8080')
 
-ws.on('open', function open () {
-  ws.send('hello')
-})
+  ws.on('open', function open () {
+    ws.send('hello')
+  })
 
-ws.on('message', function incoming (data) {
-  console.log('>' + data)
-})
+  ws.on('message', function incoming (data) {
+    console.log('>' + data)
+  })
+
+  ws.on('close', (code, reason) => {
+    log.info(`Connection closed with code ${code}: ${reason}`)
+    if (code === 1006) {
+      retry()
+    }
+  })
+  ws.on('error', e => log.error(e))
+}
+
+function retry () {
+  log.info(`Retrying in ${RETRY_INTERVAL} seconds`)
+  setTimeout(connect, RETRY_INTERVAL * 1000)
+}
 
 process.stdin.on('data', d => ws.send(d.toString()))
+
+connect()
