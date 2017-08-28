046b1f3cdbe3729737f1ebe8d018395ee56f60f1
anoff <offenhaeuser@gmail.com>
2017-08-28 21:52:09 +0200

    :scissors: refactor: add separate endpoint

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 const Server = require('./lib/server')
+const initMineEndpoint = require('./lib/endpoints-mine').initEndpoint
 global.log = require('./lib/logger').log
 global.config = require('./lib/config')
 
 const server = new Server({port: config.port})
 
-server.createEndpoint('mine', {
-  path: '/'
-})
-server.addHook('mine', 'connection', function connection (ws, req) {
-  log.info(`new client joined, total active: ${this.clients.size}`)
-  ws.send('hello you')
-  ws.on('message', function incoming (message) {
-    log.debug(`received: ${message}`)
-  })
-})
+initMineEndpoint(server)
 server.start()
