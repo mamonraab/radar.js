22d1d022d6d0a36b0048afd182b58adbc44be514
anoff <offenhaeuser@gmail.com>
2017-08-29 22:58:43 +0200

    :scissors: refactor: fix how multiple WSS endpoints are built

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ global.config = require('./lib/config')
 global.log = require('./lib/logger').log
 const Server = require('./lib/server')
 const initMineEndpoint = require('./lib/endpoint-mine').initEndpoint
+const initStatsEndpoint = require('./lib/endpoint-stats').initEndpoint
 const statistics = require('./lib/stats')
 
 const server = new Server({port: config.port})
 
 initMineEndpoint(server)
+initStatsEndpoint(server)
 server.start()
 
 statistics.startInterval()
